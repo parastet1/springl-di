add3b61862a4fefaaec6eb8d5797507be6aae0b1
Tanuj 27 <tanuj.paraste27@gmail.com>
2023-01-15 23:57:22 +0530

    Add initial files

---
+++ src/main/java/com/tp/di/SpringlDiApplication.java
@@ -0,0 +1,13 @@
+package com.tp.di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringlDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringlDiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tp/di/SpringlDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tp.di;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringlDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
