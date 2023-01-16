bc2ce94bff71f8411b08377ee420cba3127c1e54
Tanuj 27 <tanuj.paraste27@gmail.com>
2023-01-17 00:08:18 +0530

    DI with Spring: resolve injection conflict for @Autowired(optional) using @Qualifier: constructor injection

--- src/main/java/com/tp/di/controllers/ConstructedInjectedController.java
+++ src/main/java/com/tp/di/controllers/ConstructedInjectedController.java
@@ -1,13 +1,14 @@
 package com.tp.di.controllers;
 
 import com.tp.di.services.GreetingService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ConstructedInjectedController {
     private final GreetingService greetingService;
 
-    public ConstructedInjectedController(GreetingService greetingService) {
+    public ConstructedInjectedController(@Qualifier("constructorGreetingServiceImpl") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
