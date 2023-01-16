04c7178a4b889612ef85d73ab99de02c28313848
Tanuj 27 <tanuj.paraste27@gmail.com>
2023-01-16 23:38:11 +0530

    DI with Spring: constructor injection, remove @Autowired

--- src/main/java/com/tp/di/controllers/ConstructedInjectedController.java
+++ src/main/java/com/tp/di/controllers/ConstructedInjectedController.java
@@ -1,14 +1,12 @@
 package com.tp.di.controllers;
 
 import com.tp.di.services.GreetingService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ConstructedInjectedController {
     private final GreetingService greetingService;
 
-    @Autowired // this is optional for constructor injection, spring 4.2 probably
     public ConstructedInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
