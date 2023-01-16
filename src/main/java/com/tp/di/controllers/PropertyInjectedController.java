a66697e80a3b06e93c4a34f0fe6fbd147328196c
Tanuj 27 <tanuj.paraste27@gmail.com>
2023-01-17 00:07:32 +0530

    DI with Spring: resolve injection conflict for @Autowired using @Qualifier: property injection

--- src/main/java/com/tp/di/controllers/PropertyInjectedController.java
+++ src/main/java/com/tp/di/controllers/PropertyInjectedController.java
@@ -2,14 +2,16 @@ package com.tp.di.controllers;
 
 import com.tp.di.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class PropertyInjectedController {
     @Autowired
+    @Qualifier("propertyGreetingServiceImpl")
     GreetingService greetingService;
 
-    public String getGreeting(){
+    public String getGreeting() {
         return greetingService.sayGreeting();
     }
 }
